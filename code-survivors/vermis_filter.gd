ba3176e1ca05c74ec86e5ab57a833e3f9ebd97c4
Eduardo <dudu-fv@live.com>
2024-12-23 17:01:51 -0300

    added filter and test images

---
+++ code-survivors/vermis_filter.gd
@@ -0,0 +1,12 @@
+extends ColorRect
+
+func _ready():
+	material = ShaderMaterial.new()
+	material.shader = preload("res://retro_filter.gdshader")
+	mouse_filter = Control.MOUSE_FILTER_IGNORE
+	
+	# Make it cover the full viewport
+	anchor_right = 1  # Stretch to right edge
+	anchor_bottom = 1  # Stretch to bottom edge
+	offset_right = 0  # No additional offset needed
+	offset_bottom = 0
